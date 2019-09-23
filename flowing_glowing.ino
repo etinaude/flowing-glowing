4da424f2b16a14bea7a30df7f8ce56c6b5110cae
Etienne Naude <etinaude@gmail.com>
2019-09-23 19:08:56 +1200

    Add files via upload

--- flowing_glowing.ino
+++ flowing_glowing.ino
@@ -1,23 +1,41 @@
-const int numLEDS=5;
-const int lights=5;
+const int lights=4;
 const int pump=4;
 const int pot=2;
+const int speak=3;
 int onDelay =20;
 int offDelay=20;
-int value=0;
+int value=500;
 
 void setup() {  
   Serial.begin(9600);
-  Serial.println("on for: "+String(onDelay)+"   off for "+String(offDelay));
   pinMode(lights,OUTPUT);
   pinMode(pump,OUTPUT);
-  value = analogRead(pot);
+  pinMode(speak,OUTPUT);
+  digitalWrite(pump,HIGH);
 }
 
 void loop() {
-  value = analogRead(pot);
+  if(analogRead(pot)==0){
+    onDelay =0;
+    offDelay=200;  
+  }
+  else if(analogRead(pot)==1023){
+    onDelay =200;
+    offDelay = 0;
+  }else{
+   value = (15+(analogRead(pot))/20);
+   onDelay =value;
+   offDelay=value; 
+  }
+  if(onDelay>0){
   digitalWrite(lights,HIGH);
-  delay(value);
+  digitalWrite(speak,HIGH);
+  delay(onDelay);
+  }
+  if(offDelay>0){
   digitalWrite(lights,LOW);
-  delay(value);
+  digitalWrite(speak,LOW);
+  delay(offDelay);
+  }
+  Serial.println(value);
 }
